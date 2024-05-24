6d1d8edf0449eac00c12d841cd911af269e2bc34
Sergiy Ladygin <sergiyladygin@gmail.com>
2024-05-24 09:26:01 +0300

    Terraform projects to create an AWS EC2 instance with Docker and Docker Compose

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Temporary disable SELinux
+sudo echo "0" > /selinux/enforce
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
--- main.tf
+++ main.tf
@@ -1,3 +1,63 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_instance" "week2_task" {
+  ami                         = "ami-0134dde2b68fe1b07"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.week2_task.id]
+  key_name                    = aws_key_pair.ssh_key.key_name
+  user_data                   = file("./install.sh")
+
+  tags = {
+    name  = "Docker&Docker-Compose instance"
+    owner = "Sergiy Ladygin"
+  }
+}
+
+resource "aws_security_group" "week2_task" {
+  name = "Docker&Docker-Compose security group"
+  # description = ""
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["213.109.128.0/20"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name  = "Docker&Docker-Compose security group"
+    owner = "Sergiy Ladygin"
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
